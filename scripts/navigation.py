fa4047401a83523eae0aff4bbadc744cb7248fbe
Sunwolf <91574917+Sage-A@users.noreply.github.com>
2025-12-10 21:00:39 -0600

    Update navigation.py

--- scripts/navigation.py
+++ scripts/navigation.py
@@ -13,11 +13,12 @@ from math import radians
 
 class Navigation:
     def __init__(self):
+        # Initialize node and variables
         rospy.init_node('navigation')
         self.rate = rospy.Rate(10)
         self.pose = pose.Pose()
         self.turn_speed = radians(20) # degrees per second
-        self.angle_offset = 0.1
+        self.angle_offset = 0.1 # radian offset to account for rotation delay
         self.speed = 0.15 # m / s
         self.pub = rospy.Publisher('move_cmd', Twist, queue_size = 10)
         rospy.Subscriber('navi', PathSegment, self.callback)
@@ -32,19 +33,23 @@ class Navigation:
         start_location = (self.pose.current_x, self.pose.current_y)
 
         twist = Twist()
+        # Set turn direction based on angle
         if data.turn_angle > 0:
             twist.angular.z =  self.turn_speed
         else:
             twist.angular.z = -1 * self.turn_speed
 
+        # Turn until pose is offset from starting value by goal rotation
         while abs(start_angle - self.pose.current_theta) < abs(data.turn_angle) - self.angle_offset:
                 self.pub.publish(twist)
                 self.rate.sleep()
 
+        # Pause driving commands to avoid excess drift while driving forward
         for i in range(10):
             self.pub.publish(Twist())
             self.rate.sleep()
-        
+
+        # Drive forward until pose offset from starting position is greater than goal distance
         twist.angular.z = 0
         twist.linear.x = self.speed
         while path.calculate_distance(start_location, [self.pose.current_x, self.pose.current_y]) < data.distance:
